c8a477e2d8da7cc90e03e819ffe39d68ccf41635
Kyoko <dominicdomicity12@gmail.com>
2023-09-24 12:18:24 +0100

    Added personal site and moved phas guide to side

---
+++ elements/footer.php
@@ -0,0 +1,14 @@
+        <footer class="bg-white py-4 mt-auto">
+            <div class="container px-5">
+                <div class="row align-items-center justify-content-between flex-column flex-sm-row">
+                    <div class="col-auto"><div class="small m-0">Copyright &copy; 2023 Dominikos Vlachantonis</div></div>
+                    <div class="col-auto">
+                        <a class="small" href="https://www.linkedin.com/in/dominik-vlachantonis-2624001bb/"> <i class="bi bi-linkedin" style="color: black"></i> </a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="https://github.com/DominikVla"> <i class="bi bi-github" style="color: black"></i> </a>
+                        <span class="mx-1">&middot;</span>
+                        <a class="small" href="contact.php"> <i class="bi bi-person-lines-fill" style="color: black"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </footer>

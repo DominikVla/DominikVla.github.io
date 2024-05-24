750a6a7d072330b453b51423b62bef1f6de6cf05
Kyoko <74500880+DominikVla@users.noreply.github.com>
2024-05-24 08:12:42 +0100

    Update sendform.js

--- avalanche/js/sendform.js
+++ avalanche/js/sendform.js
@@ -4,8 +4,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false, // Use `true` for port 465, `false` for all other ports
   auth: {
-    user: "dominicdomicity12@gmail.com",
-    pass: "hyzt osrl byym yqgv",
+    user: "",
+    pass: "",
   },
 });
 
